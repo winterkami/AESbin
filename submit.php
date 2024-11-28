7a1b97c7d2f4faa89190f2e6597f660af23dd1ca
Enlai Li <enlai.li@mail.mcgill.ca>
2024-11-28 16:49:17 -0500

    Submit user content php

--- submit.php
+++ submit.php
@@ -1,5 +1,33 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db";
 
-$content = $_POST['content'];
-echo $content;
-?>
+try {
+    $mysqli = new mysqli($servername, $username, $password);
+    if ($mysqli->connect_error) {
+        throw new Exception($mysqli->connect_error);
+    }
+
+    // create database
+    $mysqli->query("CREATE DATABASE IF NOT EXISTS $dbname");
+    // create table
+    $mysqli->select_db($dbname);
+    $mysqli->query("
+    CREATE TABLE IF NOT EXISTS user_content (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            content TEXT NOT NULL
+        )
+    ");
+    // insert content
+    $content = $_POST['content'];
+    $mysqli->execute_query(
+        "INSERT INTO user_content (content) VALUES (?)",
+        [$content]
+    );
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    $mysqli->close();
+}
